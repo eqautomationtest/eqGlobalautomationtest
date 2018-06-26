9948337821d3cb982b1fc7a50b9a05cffb0ca38b
munusam <munusam@GRVMDEVWKSH879.uk1test.group.internal>
2018-06-26 11:39:57 +0100

    Commit chnages

--- MavenCucumberTest/src/test/java/CucumberTest/TestRunner.java
+++ MavenCucumberTest/src/test/java/CucumberTest/TestRunner.java
@@ -3,10 +3,11 @@ package CucumberTest;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import net.serenitybdd.junit.runners.SerenityRunner;
 
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features={"src/test/resources"},glue={"Cucumber.EqGlobal.StepDefinition"})
 public class TestRunner {
 
